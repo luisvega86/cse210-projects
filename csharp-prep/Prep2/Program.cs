f5f20893370de3e6923f3e733d4a6ab1a66e14bc
luisvega-wizards <liscala@wizards.us>
2023-04-21 15:06:37 -0300

    prep1 and prep2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,62 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? " );
+        int gradePercentage = int.Parse( Console.ReadLine() );
+
+        string gradeLetter = "";
+        if (gradePercentage > 90 )
+        {
+            gradeLetter = "A";
+        }
+        else if (gradePercentage >= 80 )
+        {
+            gradeLetter = "B";
+        }
+        else if (gradePercentage >= 70 )
+        {
+            gradeLetter = "C";
+        }
+        else
+        {
+            gradeLetter = "F";
+        }
+
+        
+        int lastDigit = gradePercentage % 10;
+        string gradeSign = "";
+        
+        if (gradeLetter != "F" && lastDigit >= 7)
+        {
+            gradeSign = "+";
+        }
+        else if (gradeLetter != "F" && lastDigit < 3)
+        {
+            gradeSign = "-";
+        }
+        
+        if (gradeLetter == "A" && lastDigit < 3)
+        {
+            gradeSign = "";
+        }
+        else if (gradeLetter == "F")
+        {
+            gradeSign = "";
+        }
+
+        Console.WriteLine($"Your grade is {gradeLetter}{gradeSign}");
+
+        if (gradeLetter != "F")
+        {
+            Console.WriteLine($"COngratulations, you pass the course!");
+        }
+        else 
+        {
+            Console.WriteLine($"Better luck next time.");
+            
+        }
+        
+        
+        
     }
 }
