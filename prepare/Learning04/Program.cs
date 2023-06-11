dbe60da4c90f2073102f71b57475a1d31331f88c
luisvega86 <rusonic@gmail.com>
2023-06-10 22:59:33 -0300

    learning activity 04

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        MathAssignment mathAssignment = new MathAssignment("Luis Vega", "Algebra", "Section 1.1", "1-5");
+        WritingAssignment writingAssignment = new WritingAssignment("The Impact of the bitcoin", "Luis Vega", "Economics");
+        string summary = mathAssignment.GetSummary();
+        string homeworkList = mathAssignment.GetHomeworkList();
+        string writingHomework = writingAssignment.GetWritingInformation();
+        Console.WriteLine(homeworkList);
+        Console.WriteLine(summary);
+        Console.WriteLine(writingHomework);
+        
+
     }
 }
